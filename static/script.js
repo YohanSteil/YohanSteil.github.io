98873ef5ce13770dc37e62a994c55c266210eb81
Yohan Steil <yohansteil@hotmail.fr>
2023-12-11 20:28:33 +0100

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,17 @@
+
+const imageSneakers = document.querySelectorAll('.projects__projet__img') ;
+const connaissanceslink = document.querySelector('.linksheader[href="#knowledge"]') ; 
+
+imageSneakers.forEach(image => {
+    image.addEventListener('click', () => {
+        image.classList.toggle('enlarged')
+    });
+})
+
+connaissanceslink.addEventListener('click', function(event) {
+event.preventDefault() ; 
+
+window.scrollTo({
+    behavior : 'smooth'
+})
+ })
