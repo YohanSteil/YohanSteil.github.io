9077b027a37d9e06f7290e1afe89ced9c60884f9
YohanSteil <yohansteil@hotmail.fr>
2024-03-14 15:08:56 +0100

    Ajout modification porfolio

---
+++ script.js
@@ -0,0 +1,23 @@
+
+// const imageSneakers = document.querySelectorAll('.projects__projet__img') ;
+// const connaissanceslink = document.querySelector('.linksheader[href="#knowledge"]') ; 
+
+// imageSneakers.forEach(image => {
+//     image.addEventListener('click', () => {
+//         image.classList.toggle('enlarged')
+//     });
+// })
+
+// connaissanceslink.addEventListener('click', function(event) {
+// event.preventDefault() ; 
+
+// window.scrollTo({
+//     behavior : 'smooth'
+// })
+//  })
+
+const image = document.querySelectorAll('.image')
+
+image.addEventListener('mouseenter', () => {
+    
+})
